6a013f3d6293f7c04cd38ca61007fb8ef1ff32b2
Aleksei Kvitinskii <aleksei.kvitinskii@gmail.com>
2019-05-22 14:05:47 +0300

    updated screen config and Z_HOME_BUMP_MM

--- Firmware/Marlin/src/Configuration.h
+++ Firmware/Marlin/src/Configuration.h
@@ -499,7 +499,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 325 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 320 }
 
 /**
  * Default Max Feed Rate (mm/s)
